8fca4953ca2b3ee93aae5d0b3767bbeb36e694f8
DinhHien0307 <dinhhienkhtn@gmail.com>
2017-12-26 10:46:24 +0700

    final

--- spec/PlayerSpec.php
+++ spec/PlayerSpec.php
@@ -9,11 +9,6 @@ use SebastianBergmann\ObjectReflector\InvalidArgumentException;
 
 class PlayerSpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
-        $this->shouldHaveType(Player::class);
-    }
-
     protected $name;
     protected $point;
 
